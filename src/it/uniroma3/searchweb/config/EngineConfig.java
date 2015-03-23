81021d9fed2821131f887b07bb9416b035a1b439
invisumer <kristian@10.25.254.126>
2015-03-23 10:53:01 +0100

    default paths for engine configurations

--- src/it/uniroma3/searchweb/config/EngineConfig.java
+++ src/it/uniroma3/searchweb/config/EngineConfig.java
@@ -21,9 +21,13 @@ public class EngineConfig {
 			this.indexPath = prop.getProperty("index.path");
 			this.datasetPath = prop.getProperty("dataset.path");
 		} catch (FileNotFoundException e) {
+			this.indexPath = "index";
+			this.datasetPath = "dataset";
 			e.printStackTrace();
 			logger.severe("Property file could not be found: " + e.getMessage());
 		} catch (IOException e) {
+			this.indexPath = "index";
+			this.datasetPath = "dataset";
 			e.printStackTrace();
 			logger.severe("Properties file could not be loaded: " + e.getMessage());
 		}
