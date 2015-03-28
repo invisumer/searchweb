59dbf1444fd31f12467a065f5b789f8633e97731
daniele <daniele@daniele-Aspire-X3900>
2015-03-28 17:05:45 +0100

    modified main spell checker

--- src/it/uniroma3/searchweb/engine/searcher/MainSpellChecker.java
+++ src/it/uniroma3/searchweb/engine/searcher/MainSpellChecker.java
@@ -1,25 +1,21 @@
 package it.uniroma3.searchweb.engine.searcher;
 
 import java.io.IOException;
-import java.util.List;
-
-import org.apache.lucene.search.spell.LuceneLevenshteinDistance;
-import org.apache.lucene.search.spell.NGramDistance;
 
 public class MainSpellChecker {
 	public MainSpellChecker(){}
 	public static void main(String[] args) throws IOException {
 		NaiveSpellCheckers sp = new NaiveSpellCheckers();
-//		sp.initialize();
-		int numSug = 1;
-		String query = "applw";
-		float similarity = 0.8f;
-		List<String> result = sp.getBasicSuggestions(query, numSug,similarity);
-		NGramDistance d = new NGramDistance();
-		LuceneLevenshteinDistance lld = new LuceneLevenshteinDistance();
-		for (String s : result) {
-			System.out.println("Did you mean : "+s);
-			
-		}
+		sp.initialize();
+//		int numSug = 1;
+//		String query = "applw";
+//		float similarity = 0.8f;
+//		List<String> result = sp.getBasicSuggestions(query, numSug,similarity);
+//		NGramDistance d = new NGramDistance();
+//		LuceneLevenshteinDistance lld = new LuceneLevenshteinDistance();
+//		for (String s : result) {
+//			System.out.println("Did you mean : "+s);
+//			
+//		}
 	}
 }
