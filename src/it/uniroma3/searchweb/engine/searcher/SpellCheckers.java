fddebc3b24c788168d582aedbd47614391b69bee
danrossi5 <danieler1991@gmail.com>
2015-03-30 17:39:05 +0200

    jnj

--- src/it/uniroma3/searchweb/engine/searcher/SpellCheckers.java
+++ src/it/uniroma3/searchweb/engine/searcher/SpellCheckers.java
@@ -7,6 +7,6 @@ import org.apache.lucene.index.CorruptIndexException;
 
 public interface SpellCheckers {
 	
-	public void initialize(String lang) throws CorruptIndexException, IOException;
-	public List<String> getBasicSuggestions(String query, String lang) throws IOException;
+	public void initialize(String lang, String path) throws CorruptIndexException, IOException;
+	public List<String> getBasicSuggestions(String query, String lang, String path) throws IOException;
 }
