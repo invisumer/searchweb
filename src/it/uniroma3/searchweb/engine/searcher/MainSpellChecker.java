dbb7692d19e87aff11ffddb28cb94e697031fa2a
danrossi5 <danieler1991@gmail.com>
2015-03-30 17:35:21 +0200

    un errore in meno

--- src/it/uniroma3/searchweb/engine/searcher/MainSpellChecker.java
+++ src/it/uniroma3/searchweb/engine/searcher/MainSpellChecker.java
@@ -1,18 +1,21 @@
 package it.uniroma3.searchweb.engine.searcher;
 
+import it.uniroma3.searchweb.engine.mapper.SpellCheckerMapper;
+
 import java.io.IOException;
 import java.util.List;
 
 public class MainSpellChecker {
 	public MainSpellChecker(){}
 	public static void main(String[] args) throws IOException {
-		NaiveSpellCheckers sp = new NaiveSpellCheckers();
-		sp.initialize("en");
-//		String query = "enterpreneus";
-//		List<String> result = sp.getBasicSuggestions(query, "en");
-//		for (String s : result) {
-//			System.out.println("Did you mean : "+s);
-//			
-//		}
+		SpellCheckerMapper mapper = new SpellCheckerMapper();
+		NaiveSpellCheckers sp = new NaiveSpellCheckers(mapper);
+		sp.initialize("en","html");
+		String query = "applction errop";
+		List<String> result = sp.getBasicSuggestions(query, "en","html");
+		for (String s : result) {
+			System.out.println("Did you mean : "+s);
+			
+		}
 	}
 }
