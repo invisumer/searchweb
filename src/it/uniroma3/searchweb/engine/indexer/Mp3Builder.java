2c766eff7044a422f29343e52ed5c00db067ab72
invisumer <kristian.pacquing@gmail.com>
2015-03-31 19:53:44 +0200

    new classses for WarcConverter

---
+++ src/it/uniroma3/searchweb/engine/indexer/Mp3Builder.java
@@ -0,0 +1,13 @@
+package it.uniroma3.searchweb.engine.indexer;
+
+import org.apache.lucene.document.Document;
+
+public class Mp3Builder implements DocumentBuilder {
+
+	@Override
+	public Document build(byte[] header, byte[] content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
