6775bcc608a16d113805954c275d2a8630b689fe
daniele <daniele@daniele-Aspire-X3900>
2015-04-08 18:29:07 +0200

    bug fixed in speed test

--- src/it/uniroma3/searchweb/engine/searcher/SpeedTest.java
+++ src/it/uniroma3/searchweb/engine/searcher/SpeedTest.java
@@ -1,6 +1,7 @@
 package it.uniroma3.searchweb.engine.searcher;
 
 public class SpeedTest extends Thread {
+	private static final int attempt = 56;
 	
 	public void run() {
 		makeTest();
@@ -12,7 +13,6 @@ public class SpeedTest extends Thread {
 		fields[0] = "url";
 		fields[1] = "title";
 		fields[2] = "body";
-		int attempt = 56;
 		DebuggerSearchEngine dse = new StupidSearchEngine();
 		String contentType = "html";
 		for (int i=0; i<attempt;i++)
@@ -28,19 +28,18 @@ public class SpeedTest extends Thread {
 		DebuggerSearchEngine dse = new StupidSearchEngine();
 		String contentType = "html";
 		dse.getResults(query, fields, contentType, false, "en");
-		SpeedTest st0 = new SpeedTest();
-		SpeedTest st1 = new SpeedTest();
-		SpeedTest st2 = new SpeedTest();
-		SpeedTest st3 = new SpeedTest();
+		
+		SpeedTest[] tests = new SpeedTest[Runtime.getRuntime().availableProcessors()];
+		for (int i=0; i<tests.length;i++) {
+			tests[i] = new SpeedTest();
+		}
 		double start = System.currentTimeMillis();
-		st0.start();
-		st1.start();
-		st2.start();
-		st3.start();
-		st0.join();
-		st1.join();
-		st2.join();
-		st3.join();
+		for (int i=0; i<tests.length;i++) {
+			tests[i].start();
+		}
+		for (int i=0; i<tests.length;i++) {
+			tests[i].join();;
+		}
 		
 		double stop = System.currentTimeMillis();
 		double time = stop-start;
